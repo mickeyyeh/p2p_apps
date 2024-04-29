b0949715572cd28a0a398c53f655fa74819a360a
Mickey Yeh <139152183+mickeyyeh@users.noreply.github.com>
2024-04-29 16:06:20 -0400

    Update 2_Swag_Transformer.py

--- pages/2_Swag_Transformer.py
+++ pages/2_Swag_Transformer.py
@@ -122,7 +122,8 @@ def process_file(df):
 
 def main():
     st.title("Swag File Processor")
-
+    st.write("Make sure all items has a price. (in -$00.00 format)
+        
     uploaded_file = st.file_uploader(
         "Upload your Excel file", type=["csv", "xlsx"])
 
