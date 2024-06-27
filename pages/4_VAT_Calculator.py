f27ba5ea6e1c49fa8e36aeafe36b2794900a278a
Mickey Yeh <139152183+mickeyyeh@users.noreply.github.com>
2024-06-27 11:02:27 -0400

    Update 4_VAT_Calculator.py

--- pages/4_VAT_Calculator.py
+++ pages/4_VAT_Calculator.py
@@ -1,80 +1,10 @@
-# import pandas as pd
-# import numpy as np
-# import openpyxl
-# import pycountry
-# import streamlit as st
-
-# from utils.vat_calculator.country_formatter import format_country
-
-
-# def getFile(uploaded_file, type="xlsx"):
-#     valid = ["csv", "xlsx"]
-
-#     if uploaded_file is not None:
-#         if type not in valid:
-#             raise ValueError("results: status must be one of %r." % valid)
-
-#         elif type == "csv":
-#             return pd.read_csv(uploaded_file, dtype="str")
-
-#         else:
-#             return pd.read_excel(uploaded_file, dtype="str")
-
-
-# def vatter(df):
-#     # load in vat table
-#     vat = pd.read_excel(
-#         "utils/vat_calculator/uk-eu vat rates.xlsx", engine="openpyxl")
-#     vat.rename(columns={"countrycode": "ISO_code"}, inplace=True)
-#     df = format_country(df, "Country")
-#     df = df.merge(vat, on="ISO_code")
-#     # convert column typee from str to float
-#     df['Package Value'] = df['Package Value'].astype(float)
-#     df["vat_rate"] = df["vat_rate"].astype(float)
-#     df['VAT value'] = df['Package Value'] * df["vat_rate"]
-#     df = df.loc[df['Package Value'] < 150]
-#     return df
-
-
-# # @st.cache_data
-# @st.cache
-# def convert_df(df, file_name):
-#     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-#     return df.to_excel(file_name, index=False)
-
-
-# def main():
-#     st.title("VAT Calculator")
-#     st.subheader("Please add data below")
-#     uploaded_file = st.file_uploader("Choose a file")
-#     df = getFile(uploaded_file)
-
-#     if uploaded_file:
-#         df = vatter(df)
-
-#         st.subheader("User Inputs")
-#         st.write(df)
-
-#         fileName = st.text_input("What is the file name?")
-
-#         csv_df = convert_df(df, fileName)
-#         if fileName:
-#             st.download_button(
-#                 label="Download data as Excel (xlsx)",
-#                 data=csv_df,
-#                 file_name=f'{fileName}.xlsx'
-#             )
-
-
-# if __name__ == '__main__':
-#     main()
-
 
 import pandas as pd
 import numpy as np
 import openpyxl
 import pycountry
 import streamlit as st
+
 from io import BytesIO
 
 from utils.vat_calculator.country_formatter import format_country
@@ -103,8 +33,17 @@ def vatter(df):
     df = df.merge(vat, on="ISO_code")
     # convert column typee from str to float
     df['Package Value'] = df['Package Value'].astype(float)
+    df['Actual Weight'] = df['Actual Weight'].astype(float)
+    df['DIM Weight'] = df['DIM Weight'].astype(float)
+    df['Billable Weight'] = df['Billable Weight'].astype(float)
+    df['Length'] = df['Length'].astype(float)
+    df['Height'] = df['Height'].astype(float)
+    df['Width'] = df['Width'].astype(float)
+    df['Transits - Processed To Stop Event (Business Days)'] = df['Transits - Processed To Stop Event (Business Days)'].astype(float)
+    df['Transits - Processed To Stop Event (Calendar Days)'] = df['Transits - Processed To Stop Event (Calendar Days)'].astype(float)
     df["vat_rate"] = df["vat_rate"].astype(float)
     df['VAT value'] = df['Package Value'] * df["vat_rate"]
+    df['VAT value'] = df['VAT value'].apply(lambda x: roundUP(x, 2)
     df = df.loc[df['Package Value'] < 150]
     return df
 
@@ -116,6 +55,14 @@ def convert_df(df):
     writer.save()
     processed_data = output.getvalue()
     return processed_data
+
+
+def roundUp(number, ndigits=0):
+    """Always round off"""
+    exp = number * 10 ** ndigits
+    if abs(exp) - abs(math.floor(exp)) < 0.5:
+        return type(number)(math.floor(exp) / 10 ** ndigits)
+    return type(number)(math.ceil(exp) / 10 ** ndigits)
 
 
 def main():
