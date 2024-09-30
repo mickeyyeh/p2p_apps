6d8d65f02bb00f7d8f27d77945cb8bf8189a1d12
Mickey Yeh <139152183+mickeyyeh@users.noreply.github.com>
2024-09-30 10:42:58 -0400

    Update 4_VAT_Calculator.py

--- pages/4_VAT_Calculator.py
+++ pages/4_VAT_Calculator.py
@@ -84,9 +84,9 @@ def getFile(uploaded_file: Optional[st.runtime.uploaded_file_manager.UploadedFil
         if file_type not in valid:
             raise ValueError(f"File type must be one of {valid}.")
         elif file_type == "csv":
-            return pd.read_csv(uploaded_file)
+            return pd.read_csv(uploaded_file, dtype=str)
         else:
-            return pd.read_excel(uploaded_file)
+            return pd.read_excel(uploaded_file, dtype=str)
     else:
         return None
 
@@ -99,17 +99,64 @@ def vatter(df: pd.DataFrame) -> pd.DataFrame:
     vat.rename(columns={"countrycode": "ISO_code"}, inplace=True)
     df = format_country(df, "Country")
     df = df.merge(vat, on="ISO_code")
+    # convert column format
+    df['Package Value'] = df['Package Value'].astype(float)
+    df["vat_rate"] = df["vat_rate"].astype(float)
     df['VAT value'] = df['Package Value'] * df["vat_rate"]
     df = df.loc[df['Package Value'] < 150]
     return df
 
 
+# @st.cache  # Consider using @st.cache_data if using Streamlit version >= 1.18
+# def convert_df(df: pd.DataFrame) -> str:
+#     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+#     return df.to_csv(index=False)
+
 @st.cache  # Consider using @st.cache_data if using Streamlit version >= 1.18
-def convert_df(df: pd.DataFrame) -> str:
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv(index=False)
+def convert_df_to_excel(df: pd.DataFrame) -> BytesIO:
+    # Cache the conversion to prevent computation on every rerun
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='openpyxl')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.save()
+    output.seek(0)  # Set cursor back to the beginning of the file
+    return output
 
 
+# # Download as CSV
+# def main() -> None:
+#     st.title("VAT Calculator")
+#     st.subheader("Please add data below")
+#     uploaded_file = st.file_uploader("Choose a file")
+
+#     if uploaded_file:
+#         df = getFile(uploaded_file)
+#         if df is not None:
+#             df = vatter(df)
+
+#             st.subheader("User Inputs")
+#             st.write(df)
+
+#             file_name = st.text_input("What is the file name?")
+
+#             csv = convert_df(df)
+#             if file_name:
+#                 st.download_button(
+#                     label="Download data as CSV",
+#                     data=csv,
+#                     file_name=f'{file_name}.csv'
+#                 )
+#         else:
+#             st.error(
+#                 "Failed to read the uploaded file. Please check the file format.")
+#     else:
+#         st.info("Please upload a CSV or Excel file to proceed.")
+
+
+# if __name__ == '__main__':
+#     main()
+
+# Download as Excel
 def main() -> None:
     st.title("VAT Calculator")
     st.subheader("Please add data below")
@@ -125,12 +172,13 @@ def main() -> None:
 
             file_name = st.text_input("What is the file name?")
 
-            csv = convert_df(df)
+            excel_data = convert_df_to_excel(df)
             if file_name:
                 st.download_button(
-                    label="Download data as CSV",
-                    data=csv,
-                    file_name=f'{file_name}.csv'
+                    label="Download data as Excel",
+                    data=excel_data,
+                    file_name=f'{file_name}.xlsx',
+                    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
                 )
         else:
             st.error(
